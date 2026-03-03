97524c1b80d0ae853aa1a617674ceee2945387a2
legoj15 <legoj15@yahoo.com>
2026-03-02 22:00:36 -0500

    Port buildcraft-lib Marker System bases

--- buildcraft-lib/src/main/java/buildcraft/lib/marker/MarkerCache.java
+++ buildcraft-lib/src/main/java/buildcraft/lib/marker/MarkerCache.java
@@ -1,2 +1,79 @@
+/*
+ * Copyright (c) 2017 SpaceToad and the BuildCraft team
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/
+ */
+
 package buildcraft.lib.marker;
-public abstract class MarkerCache {}
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
+import net.minecraft.resources.ResourceKey;
+
+import buildcraft.api.core.BCDebugging;
+import buildcraft.api.core.BCLog;
+
+public abstract class MarkerCache<S extends MarkerSubCache<?>> {
+    public static final boolean DEBUG = BCDebugging.shouldDebugLog("lib.markers");
+    public static final List<MarkerCache<?>> CACHES = new ArrayList<>();
+
+    public final String name;
+
+    private final Map<ResourceKey<Level>, S> cacheClient = new ConcurrentHashMap<>();
+    private final Map<ResourceKey<Level>, S> cacheServer = new ConcurrentHashMap<>();
+
+    public MarkerCache(String name) {
+        this.name = name;
+    }
+
+    public static void registerCache(MarkerCache<?> cache) {
+        CACHES.add(cache);
+        if (DEBUG) {
+            BCLog.logger
+                    .info("[lib.markers] Registered a cache " + cache.name + " with an ID of " + (CACHES.size() - 1));
+        }
+    }
+
+    public static void postInit() {
+        if (DEBUG) {
+            BCLog.logger.info("[lib.markers] Sorted list of cache types:");
+            for (int i = 0; i < CACHES.size(); i++) {
+                final MarkerCache<?> cache = CACHES.get(i);
+                BCLog.logger.info("  " + i + " = " + cache.name);
+            }
+            BCLog.logger.info("[lib.markers] Total of " + CACHES.size() + " cache types");
+        }
+    }
+
+    public static void onPlayerJoinWorld(ServerPlayer player) {
+        for (MarkerCache<?> cache : CACHES) {
+            Level world = player.level();
+            cache.getSubCache(world).onPlayerJoinWorld(player);
+        }
+    }
+
+    public static void onWorldUnload(Level world) {
+        for (MarkerCache<?> cache : CACHES) {
+            cache.onWorldUnloadImpl(world);
+        }
+    }
+
+    private void onWorldUnloadImpl(Level world) {
+        Map<ResourceKey<Level>, S> cache = world.isClientSide() ? cacheClient : cacheServer;
+        ResourceKey<Level> key = world.dimension();
+        cache.remove(key);
+    }
+
+    protected abstract S createSubCache(Level world);
+
+    public S getSubCache(Level world) {
+        Map<ResourceKey<Level>, S> cache = world.isClientSide() ? cacheClient : cacheServer;
+        ResourceKey<Level> key = world.dimension();
+        return cache.computeIfAbsent(key, k -> createSubCache(world));
+    }
+}
--- buildcraft-lib/src/main/java/buildcraft/lib/tile/TileMarker.java
+++ buildcraft-lib/src/main/java/buildcraft/lib/tile/TileMarker.java
@@ -1,2 +1,85 @@
 package buildcraft.lib.tile;
-public abstract class TileMarker {}
+
+import java.util.List;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+
+import buildcraft.api.tiles.IDebuggable;
+
+import buildcraft.lib.marker.MarkerCache;
+import buildcraft.lib.marker.MarkerConnection;
+import buildcraft.lib.marker.MarkerSubCache;
+
+public abstract class TileMarker<C extends MarkerConnection<C>> extends BlockEntity implements IDebuggable {
+
+    public TileMarker(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    public abstract MarkerCache<? extends MarkerSubCache<C>> getCache();
+
+    public MarkerSubCache<C> getLocalCache() {
+        return getCache().getSubCache(level);
+    }
+
+    /**
+     * @return True if this has lasers being emitted. Activates the surrounding
+     *         "glow"
+     *         parts for the block model.
+     */
+    public abstract boolean isActiveForRender();
+
+    public C getCurrentConnection() {
+        if (level == null)
+            return null;
+        return getLocalCache().getConnection(getBlockPos());
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        if (level != null && !level.isClientSide()) {
+            getLocalCache().loadMarker(getBlockPos(), this);
+        }
+    }
+
+    @Override
+    public void setRemoved() {
+        super.setRemoved();
+        if (level != null && !level.isClientSide()) {
+            getLocalCache().unloadMarker(getBlockPos());
+        }
+    }
+
+    public void onRemove() {
+        if (level != null && !level.isClientSide()) {
+            getLocalCache().removeMarker(getBlockPos());
+        }
+    }
+
+    protected void disconnectFromOthers() {
+        C currentConnection = getCurrentConnection();
+        if (currentConnection != null) {
+            currentConnection.removeMarker(getBlockPos());
+        }
+    }
+
+    @Override
+    public void getDebugInfo(List<String> left, List<String> right, Direction side) {
+        if (level == null)
+            return;
+        C current = getCurrentConnection();
+        MarkerSubCache<C> cache = getLocalCache();
+        left.add("Exists = " + (cache.getMarker(getBlockPos()) == this));
+        if (current == null) {
+            left.add("Connection = null");
+        } else {
+            left.add("Connection:");
+            current.getDebugInfo(getBlockPos(), left);
+        }
+    }
+}

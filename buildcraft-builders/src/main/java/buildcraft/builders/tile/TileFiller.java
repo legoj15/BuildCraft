c19b9d56a61052c7eb7d490377506aa87e42e127
legoj15 <legoj15@yahoo.com>
2026-03-08 20:10:32 -0400

    Add filler block localization string and fix translation key

--- buildcraft-builders/src/main/java/buildcraft/builders/tile/TileFiller.java
+++ buildcraft-builders/src/main/java/buildcraft/builders/tile/TileFiller.java
@@ -276,7 +276,7 @@ public class TileFiller extends TileBC_Neptune
 
     @Override
     public Component getDisplayName() {
-        return Component.translatable("tile.buildcraftbuilders.filler.name");
+        return Component.translatable("block.buildcraftbuilders.filler");
     }
 
     @Nullable

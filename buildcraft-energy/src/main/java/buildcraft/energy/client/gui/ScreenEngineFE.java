4f2cc918f4396f6970423b18aa5e889b2bb72e9d
legoj15 <legoj15@yahoo.com>
2026-04-07 17:16:51 -0400

    Restore upgrade tooltips to ScreenDynamoMJ and ScreenEngineFE
    Added GuiElementSimple tooltip regions indicating compatible upgrade gear properties. Rendering of the physical proxy gear items has been deferred due to missing GuiGraphics 1.21.3+ Item renderer bindings.

--- buildcraft-energy/src/main/java/buildcraft/energy/client/gui/ScreenEngineFE.java
+++ buildcraft-energy/src/main/java/buildcraft/energy/client/gui/ScreenEngineFE.java
@@ -17,6 +17,7 @@ public class ScreenEngineFE extends GuiBC8<ContainerEngineFE> {
     private static final Identifier TEXTURE = Identifier.parse("buildcraftenergy:textures/gui/rf_engine_gui.png");
     private static final int SIZE_X = 176, SIZE_Y = 177;
     private static final GuiIcon ICON_GUI = new GuiIcon(TEXTURE, 0, 0, SIZE_X, SIZE_Y);
+    private static final buildcraft.lib.gui.pos.GuiRectangle RECT_UPGRADE_TYPES = new buildcraft.lib.gui.pos.GuiRectangle(42, 20, 74, 20);
 
     public ScreenEngineFE(ContainerEngineFE menu, Inventory playerInv, Component title) {
         super(menu, playerInv, title, SIZE_X, SIZE_Y);
@@ -40,12 +41,42 @@ public class ScreenEngineFE extends GuiBC8<ContainerEngineFE> {
             ));
 
             mainGui.shownElements.add(new LedgerHelp(mainGui, false));
+            
+            mainGui.shownElements.add(new buildcraft.lib.gui.GuiElementSimple(mainGui, RECT_UPGRADE_TYPES.offset(mainGui.rootElement)) {
+                @Override
+                public void addToolTips(java.util.List<buildcraft.lib.gui.elem.ToolTip> tooltips) {
+                    if (contains(mainGui.mouse)) {
+                        java.util.List<String> lines = new java.util.ArrayList<>();
+                        lines.add(LocaleUtil.localize("buildcraft.gui.rf_engine.upgrade_types"));
+                        for (java.util.Map.Entry<net.minecraft.world.item.Item, Long> entry : buildcraft.energy.tile.TileEngineFE.UPGRADE_VALUES.entrySet()) {
+                            String itemName = new net.minecraft.world.item.ItemStack(entry.getKey()).getHoverName().getString();
+                            long mj = entry.getValue();
+                            int rf = (int) (mj / 10L); // 10 MJ = 1 RF fallback if conversion object missing
+                            lines.add(itemName + " = +" + mj + " MJ/t (" + rf + " RF/t)");
+                        }
+                        tooltips.add(new buildcraft.lib.gui.elem.ToolTip(lines));
+                    }
+                }
+            });
         }
     }
 
     @Override
     protected void drawBackgroundTexture(GuiGraphicsExtractor graphics) {
         ICON_GUI.drawAt(mainGui.rootElement);
+        
+        int x = (int) mainGui.rootElement.getX();
+        int y = (int) mainGui.rootElement.getY();
+        
+        graphics.blit(
+            net.minecraft.client.renderer.RenderPipelines.GUI_TEXTURED, TEXTURE,
+            x + 58, y + 18,
+            39.0f, 18.0f,
+            80, 23,
+            80, 23,
+            256, 256,
+            0xA5FFFFFF
+        );
     }
 
     @Override
--- changelog.md
+++ changelog.md
@@ -2,3 +2,4 @@
 - Fixed GuiBC8 rendering double-titles by suppressing vanilla AbstractContainerScreen extractLabels
 - Fixed MJ Dynamo and FE Engine GUI translation strings, height cutoffs, and label offsets
 - Fixed translation strings and cut-off rendering in ScreenDynamoMJ and ScreenEngineFE by reverting SIZE_Y parameters and targeting correct localization keys
+- Restored upgrade tooltip functionality and upgrade overlay to ScreenDynamoMJ and ScreenEngineFE
